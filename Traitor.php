b0ca11cb80cc4b20d76cca46790e5a61c5a318c2
sam <sammy@hexmakina.be>
2021-08-29 18:06:52 +0200

    removed: public var (appeared in traited objects) replaced: search and execute call is traitor()

--- Traitor.php
+++ Traitor.php
@@ -4,17 +4,16 @@ namespace HexMakina\Traitor;
 
 trait Traitor
 {
-    public $traitor_pattern = '/.+Traitor_%s$/';
-
-    public function search_and_execute_trait_methods($method_name)
+    public function traitor($method_name)
     {
-        $pattern = sprintf($this->traitor_pattern, $method_name);
+        $regex = sprintf('/.+Traitor_%s$/', $method_name);
         $errors = [];
         $traits = (new \ReflectionClass($this))->getTraitNames();
+
         foreach ($traits as $trait_name) {
             $trait_methods = (new \ReflectionClass($trait_name))->getMethods();
             foreach ($trait_methods as $method) {
-                if (preg_match($pattern, $method->name, $match) === 1) {
+                if (preg_match($regex, $method->name, $match) === 1) {
                     $callable = current($match);
                     $errors ["$trait_name::".$method->name]= $this->$callable();
                 }
